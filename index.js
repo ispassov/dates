2cf7440982b8c68323f9bbfe6715a7eaec01aeef
ispassov <61962940+ispassov@users.noreply.github.com>
2022-12-02 10:14:57 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 
-let date = new Date('1 January 2022')
-date.setDate(date.getDate() + 329)
+/* let date = new Date('1 January 2022')
+date.setDate(date.getDate() + 329) */
 
-let month = date.toLocaleString().split('/')[0]
+/* let month = date.toLocaleString().split('/')[0]
 let year = date.getFullYear()
 let day = date.toLocaleString().split('/')[1]
-let monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+let monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] */
 
-let firstDate = new Date(document.querySelector('.first-date').innerHTML)
-let secondDate = new Date(document.querySelector('.second-date').value)
+let firstDate = document.querySelector('.first-date')
+let secondDate = document.querySelector('.second-date')
+let btnCalculate = document.querySelector('.btn-calculate')
+let days = document.querySelector('.days')
 
 /* console.log(
     day,
@@ -18,13 +20,12 @@ let secondDate = new Date(document.querySelector('.second-date').value)
     year
 ) */
 
-document.querySelector('.first-date').addEventListener('input', () => {
-    console.log(firstDate)
+btnCalculate.addEventListener('click', () => {
+        /* Math.floor((((((secondDate.getTime() - firstDate.getTime()) / 1000)/60)/60)/24)),
+        Math.floor((((((secondDate.getTime() - firstDate.getTime()) / 1000)/60)/60))),
+        Math.floor((((((secondDate.getTime() - firstDate.getTime()) / 1000)/60)))) */
+        days.classList.remove('d-none')
+        days.innerHTML = `Days: ${Math.floor((((((new Date(secondDate.value).getTime() - new Date(firstDate.value).getTime()) / 1000)/60)/60)/24))}`
 })
 
 
-console.log(
-    Math.floor((((((secondDate.getTime() - firstDate.getTime()) / 1000)/60)/60)/24)),
-    Math.floor((((((secondDate.getTime() - firstDate.getTime()) / 1000)/60)/60))),
-    Math.floor((((((secondDate.getTime() - firstDate.getTime()) / 1000)/60))))
-)
